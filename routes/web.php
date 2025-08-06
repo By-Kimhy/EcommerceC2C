2f897469bf5d87fce77c234df0830b56a77fa0f4
Kim hy <kimhyby99@gmail.com>
2025-08-06 11:48:27 +0700

    commit project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\frontend\CartController;
+use App\Http\Controllers\frontend\CheckOutController;
+use App\Http\Controllers\frontend\ComfirmController;
+use App\Http\Controllers\frontend\ContactController;
+use App\Http\Controllers\frontend\HomeController;
+use App\Http\Controllers\frontend\ProductController;
+use App\Http\Controllers\frontend\ProductDetailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Frontend Block
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+});
+
+Route::controller(CartController::class)->group(function () {
+    Route::get('/cart', 'index')->name('cart');
+});
+
+Route::controller(CheckOutController::class)->group(function () {
+    Route::get('/checkout', 'index')->name('checkout');
+});
+
+Route::controller(ComfirmController::class)->group(function () {
+    Route::get('/comfirm', 'index')->name('comfirm');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/product', 'index')->name('product');
+});
+
+Route::controller(ProductDetailController::class)->group(function () {
+    Route::get('/productDetail', 'index')->name('productDetail');
+});
+
+Route::controller(ContactController::class)->group(function () {
+    Route::get('/contact', 'index')->name('contact');
 });
